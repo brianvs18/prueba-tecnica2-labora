9359fc38a3078b414a8b9d943520454c9b69fe49
Brian Vanegas <brianvs18@gmail.com>
2024-05-02 19:18:13 -0500

    create unit testing

--- creative-projects-management-system/usecases/commands/delete-project/delete_project_test.go
+++ creative-projects-management-system/usecases/commands/delete-project/delete_project_test.go
@@ -2,14 +2,30 @@ package delete_project
 
 import (
 	"bufio"
-	"strings"
+	"bytes"
+	"ejercicio-poo/creative-projects-management-system/common/constants"
+	"ejercicio-poo/creative-projects-management-system/entities"
+	get_projects "ejercicio-poo/creative-projects-management-system/usecases/queries/get-projects"
+	"reflect"
 	"testing"
 )
 
 func TestRun(t *testing.T) {
 
 	input := "ebDV8ycct7Yg0on975MTu"
-	scan := bufio.NewScanner(strings.NewReader(input))
+	expectedProject := entities.ProjectBuilder(input,
+		"Digital Frontier",
+		"A project to create digital services across the world",
+		[]string{"WbvcnLmqvUS3vaHVJe6Jo"},
+		constants.ACTIVE)
+	scan := bufio.NewScanner(bytes.NewBufferString(input))
 
+	//var output bytes.Buffer
 	Run(scan)
+	project, _ := get_projects.FindOne(input)
+
+	if reflect.DeepEqual(*project, *expectedProject) {
+		t.Errorf("FindOne(%s) \n expected %s \n got %s", input, expectedProject, project)
+	}
+
 }
